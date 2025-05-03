2edb5c27eed9417cce312f13bdcafcc9e793c071
pelonidas <jannahalka@icloud.com>
2025-05-03 18:28:13 +0200

    refactor the train file to class

--- models/baseline_model/train.py
+++ models/baseline_model/train.py
@@ -3,21 +3,13 @@ from transformers import (
     AutoTokenizer,
     AutoModelForTokenClassification,
     DataCollatorForTokenClassification,
-    PreTrainedTokenizerFast,
 )
 from datasets import load_dataset, DatasetDict
 from torch.utils.data import DataLoader
 import torch
 from tqdm import tqdm
-from transformers.tokenization_utils_base import BatchEncoding
 
-MODEL = "google-bert/bert-base-cased"
-LR = 2e-5
 EPOCHS = 3
-LABEL_COLUMN = "labels"
-BATCH_SIZE = 8
-IGNORE_LABEL = -100
-
 
 def get_dataset():
     dataset = load_dataset("jannahalka/nlp-project-data", trust_remote_code=True)
@@ -28,115 +20,121 @@ def get_dataset():
     return dataset
 
 
+class BaselineModelTrainer:
+    def __init__(
+        self,
+        dataset: DatasetDict,
+        model_name="google-bert/bert-base-cased",
+        learning_rate=2e-5,
+        batch_size=8,
+    ):
+        self.batch_size = batch_size
+
+        train, dev, test = dataset.values()
+        self.train_dataset, self.dev_dataset, self.test_dataset = train, dev, test
+
+        labels: list[str] = train.features["labels"].feature.names
+        labels_size = len(labels)
+
+        config = AutoConfig.from_pretrained(model_name, num_labels=labels_size)
+
+        self.model = AutoModelForTokenClassification.from_pretrained(
+            model_name, config=config
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+        self.data_collator = DataCollatorForTokenClassification(self.tokenizer)
+
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model.to(self.device)
+
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=learning_rate)
+
+    def train(self, epochs: int):
+        self.prepare_data()
+
+        for epoch in range(epochs):
+            self.train_epoch(epoch)
+
+    def train_epoch(self, epoch: int):
+        self.model.train()
+
+        train_loader = tqdm(
+            enumerate(self.train_loader),
+            total=len(self.train_loader),
+            desc=f"Training Epoch {epoch + 1}",
+        )
+
+        for step, batch in train_loader:
+            batch = {key: value.to(self.device) for key, value in batch.items()}
+
+            output = self.model(**batch)
+            loss = output.loss
+
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+    def prepare_data(self):
+        self.train_dataset = self.train_dataset.map(
+            self.tokenize,
+            batched=True,
+            remove_columns=self.train_dataset.column_names,
+            desc="Running tokenizer on the dataset",
+        )
+
+        self.train_loader = DataLoader(
+            self.train_dataset,
+            shuffle=True,
+            collate_fn=self.data_collator,
+            batch_size=self.batch_size,
+        )
+
+    def tokenize(self, examples):
+        tokenized_inputs = self.tokenizer(
+            examples["tokens"],
+            max_length=128,  # Max length of a batch
+            padding=False,
+            truncation=True,
+            is_split_into_words=True,
+        )
+        labels = self.align_labels(examples, tokenized_inputs)
+        tokenized_inputs["labels"] = labels
+        return tokenized_inputs
+
+    def align_labels(self, examples, tokenized_inputs):
+        labels = []
+
+        for batch_index, original_labels in enumerate(examples[LABEL_COLUMN]):
+            word_ids = tokenized_inputs.word_ids(batch_index=batch_index)
+            masked_labels = self.create_labels_mask(word_ids, original_labels)
+            labels.append(masked_labels)
+
+        return labels
+
+    def create_labels_mask(self, word_ids, original_labels):
+        IGNORE_LABEL = -100
+        masked = []
+        prev_word_id: int | None = None
+
+        for word_id in word_ids:
+            if word_id is None or word_id == prev_word_id:
+                masked.append(IGNORE_LABEL)
+            else:
+                label = original_labels[word_id]
+                masked.append(label)
+
+            prev_word_id = word_id
+        return masked
+
+    def save(self, path: str):
+        self.model.save_pretrained(path)
+        self.tokenizer.save_pretrained(path)
+
+
+
+
 dataset = get_dataset()
-train, dev, test = dataset.values()
 
-labels: list[str] = train.features["labels"].feature.names
-
-tokenizer: PreTrainedTokenizerFast = AutoTokenizer.from_pretrained(MODEL, use_fast=True)
-config = AutoConfig.from_pretrained(MODEL, num_labels=len(labels))
-model = AutoModelForTokenClassification.from_pretrained(MODEL, config=config)
-
-
-def create_labels_mask(word_ids: list[int | None], original_labels):
-    masked = []
-    prev_word_id: int | None = None
-
-    for word_id in word_ids:
-        if word_id is None or word_id == prev_word_id:
-            masked.append(IGNORE_LABEL)
-        else:
-            label = original_labels[word_id]
-            masked.append(label)
-
-        prev_word_id = word_id
-    return masked
-
-
-def align_batch_labels(examples, tokenized_inputs: BatchEncoding):
-    masked_labels = []
-
-    for batch_index, original_labels in enumerate(examples[LABEL_COLUMN]):
-        word_ids = tokenized_inputs.word_ids(batch_index=batch_index)
-        masked_labels = create_labels_mask(word_ids, original_labels)
-        masked_labels.append(masked_labels)
-
-    return masked_labels
-
-
-def tokenize(examples):  # TODO: Fix the type of `examples`
-    tokenized_inputs = tokenizer(
-        examples["tokens"],
-        max_length=128,  # Max length of a batch
-        padding=False,
-        truncation=True,
-        is_split_into_words=True,
-    )
-    labels = align_batch_labels(examples, tokenized_inputs)
-    tokenized_inputs["labels"] = labels
-    return tokenized_inputs
-
-
-dataset = dataset.map(
-    tokenize,
-    batched=True,
-    remove_columns=train.column_names,
-    desc="Running tokenizer on the dataset",
-)
-
-train, dev, test = dataset.values()
-
-data_collator = DataCollatorForTokenClassification(tokenizer)
-
-train_dataloader = DataLoader(
-    train, shuffle=True, collate_fn=data_collator, batch_size=BATCH_SIZE
-)
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model.to(device)
-optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
-
-
-# TODO: Refactor -> Too many arguments
-def show_loss(loss, step, batch, size):
-    if step % 100 == 0:
-        loss, current = loss.item(), step * BATCH_SIZE + len(batch)
-        print(f"Loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-
-# TODO: Refactor -> Too many arguments
-def train_loop(model, epoch, train_loader):
-    size = len(train_loader.dataset)
-
-    model.train()
-
-    train_loader = tqdm(
-        enumerate(train_loader),
-        total=len(train_loader),
-        desc=f"Training Epoch {epoch + 1}",
-    )
-
-    for step, batch in train_loader:
-        batch = {key: value.to(device) for key, value in batch.items()}
-
-        output = model(**batch)
-        loss = output.loss
-
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        show_loss(loss, step, batch, size)
-
-
-for epoch in range(EPOCHS):
-    train_loop(model, train_dataloader, epoch)
-
-
-# TODO: Figure out `model` type, Too many arguments
-def save_model(model, tokenizer: PreTrainedTokenizerFast, output_path: str):
-    model.save_pretrained(output_path)
-    tokenizer.save_pretrained(output_path)
-
-
-save_model(model, tokenizer, "./models/baseline_model/output")
+trainer = BaselineModelTrainer(dataset)
+trainer.train(EPOCHS)
+trainer.save("./models/baseline_model/output")
