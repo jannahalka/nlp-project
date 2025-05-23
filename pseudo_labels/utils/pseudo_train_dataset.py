a938737b116889cbb9ff009c18c294567e6d42b6
pelonidas <jannahalka@icloud.com>
2025-05-23 08:08:39 +0200

    Silver data only test

--- pseudo_labels/utils/pseudo_train_dataset.py
+++ pseudo_labels/utils/pseudo_train_dataset.py
@@ -4,12 +4,12 @@ from .gold_dataset import convert_annotated_data_to_dict
 
 class PseudoTrainDataset(Dataset):
     def __init__(self, silver_data: dict):
-        gold_data = convert_annotated_data_to_dict(
-            "./pseudo_labels/data/gold/annotated_train.jsonl"
-        )
-        all_tokens = silver_data["tokens"] + gold_data["tokens"]
-        all_labels = silver_data["labels"] + gold_data["labels"]
-        self.data = {"tokens": all_tokens, "labels": all_labels}
+        # gold_data = convert_annotated_data_to_dict(
+        #     "./pseudo_labels/data/gold/annotated_train.jsonl"
+        # )
+        # all_tokens = silver_data["tokens"] + gold_data["tokens"]
+        # all_labels = silver_data["labels"] + gold_data["labels"]
+        self.data = silver_data  # {"tokens": all_tokens, "labels": all_labels}
 
     def __len__(self):
         return len(self.data["labels"])
